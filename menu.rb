da38a4c02a7b7c0b8820f96ee051b8980598ee4c
Samuel Silva <samuelsilvawb@gmail.com>
2024-09-11 20:39:55 +0100

    feat: create menu

---
+++ menu.rb
@@ -0,0 +1,7 @@
+def print_menu
+  puts "What would you like to do?"
+  puts "1. Add new service credentials"
+  puts "2. Retrieve an existing service credentials"
+  puts "3. Print all credentials"
+  puts "4. Exit"
+end
